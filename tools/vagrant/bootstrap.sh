61486a42ce40a29ce0627d88b0e094b9c5a0222c
Edward Kim <onward.edkim@gmail.com>
2016-02-11 11:40:34 +1100

    add libuv as deps

--- tools/vagrant/bootstrap.sh
+++ tools/vagrant/bootstrap.sh
@@ -5,6 +5,18 @@ sudo apt-get update
 # DNX prerequisites
 sudo apt-get install -y unzip curl libunwind8 gettext libssl-dev libcurl4-openssl-dev zlib1g libicu-dev uuid-dev
 
+# install libuv for KestrelHttpServer
+sudo apt-get install automake libtool
+
+curl -sSL https://github.com/libuv/libuv/archive/v1.4.2.tar.gz | sudo tar zxfv - -C /usr/local/src
+cd /usr/local/src/libuv-1.4.2
+sudo sh autogen.sh
+sudo ./configure
+sudo make
+sudo make install
+sudo rm -rf /usr/local/src/libuv-1.4.2 && cd ~/
+sudo ldconfig
+
 # install DNVM
 curl -sSL https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.sh | DNX_BRANCH=dev sh && source ~/.dnx/dnvm/dnvm.sh
 
